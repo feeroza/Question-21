bf093226267bb96cb90f3425296ec5da4270d3d1
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:15:56 +0500

    Add files via upload

---
+++ Question 21/021.js
@@ -0,0 +1,8 @@
+//21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+var student1 = {
+    name: "ahmed",
+    age: 22,
+    rollno: 4765,
+    gender: "male"
+};
+console.log(student1);
